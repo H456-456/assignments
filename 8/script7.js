c3ee1d49ea023bc7d924ef3f81563cdc44c1a060
H456-456 <mikpkk1@gmail.com>
2025-05-08 14:08:20 +0300

    Update script7.js

--- 8/script7.js
+++ 8/script7.js
@@ -6,7 +6,7 @@ var lineNumber = 0
 
 universitySearchButton.addEventListener('click',function() {
 	fetch(
-		`http://universities.hipolabs.com/search?country=${enteringTheCountryName.value}`
+		`https://universities.hipolabs.com/search?country=${enteringTheCountryName.value}`
 	)
 		.then(response => response.json())
 		.then(data => {
